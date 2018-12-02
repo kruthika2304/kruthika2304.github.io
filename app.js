c0451ffb2c07bc93a7ed4cfc4364ba38beea478c
kruthika2304 <kruthika2304@gmail.com>
2018-12-02 11:54:03 +0530

    new4 commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module("HangmanApp",[]);
 app.controller("GameController",['$scope','$timeout',function($scope,$timeout){
-	var words=["Apple","Mango","Mousambi","Pears"];
+	var words=["Altasian","Remember","Mountain","Pokemon"];
 	$scope.incorrectLettersChosen=[];
 	$scope.correctLettersChosen=[];
 	var selectedWord='';
